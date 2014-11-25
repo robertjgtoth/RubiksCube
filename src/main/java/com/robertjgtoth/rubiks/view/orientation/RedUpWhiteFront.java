f9f50c55f23deb875db00cc03309778ed1502ac9
robertjgtoth <robertjgtoth@gmail.com>
2014-11-25 16:41:13 -0500

    Update RedUpWhiteFront.java

--- src/main/java/com/robertjgtoth/rubiks/view/orientation/RedUpWhiteFront.java
+++ src/main/java/com/robertjgtoth/rubiks/view/orientation/RedUpWhiteFront.java
@@ -3,6 +3,8 @@ package com.robertjgtoth.rubiks.view.orientation;
 import com.robertjgtoth.rubiks.view.Colors;
 import com.robertjgtoth.rubiks.view.Move;
 
+import java.util.LinkedHashMap;
+
 /**
  * Created by rtoth on 11/25/2014.
  */
@@ -19,13 +21,17 @@ public class RedUpWhiteFront extends Orientation {
     {
         switch (move)
         {
-            case DOWN:
-                return new RedUpWhiteFront(); // TODO: YellowUpRedFront
             case UP:
                 return new WhiteUpOrangeFront();
+            case DOWN:
+                return new YellowUpRedFront();
             case LEFT:
-                return new RedUpGreenFront();
+                return new GreenUpWhiteFront();
             case RIGHT:
+                return new BlueUpWhiteFront();
+            case CLOCK:
+                return new RedUpGreenFront();
+            case COUNTER_CLOCK:
                 return new RedUpBlueFront();
             default:
                 throw new IllegalArgumentException("This should never happen");
