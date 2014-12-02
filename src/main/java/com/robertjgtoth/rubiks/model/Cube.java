73f4998bb35ac27541ec8a6ac62107b055444076
robertjgtoth <robertjgtoth@gmail.com>
2014-12-02 18:22:56 -0500

    Fixed full-cube rotation after applying moves

--- src/main/java/com/robertjgtoth/rubiks/model/Cube.java
+++ src/main/java/com/robertjgtoth/rubiks/model/Cube.java
@@ -64,6 +64,14 @@ public class Cube {
         cubies[26] = new Cube3D(new Point3f(2.0f, -2.0f, 2.0f));
     }
 
+    public Cube(Cube other)
+    {
+        for (int i = 0; i < cubies.length; i++)
+        {
+            cubies[i] = new Cube3D(other.cubies[i]);
+        }
+    }
+
     public Cube3D[] getCubies()
     {
         return cubies;
@@ -95,6 +103,171 @@ public class Cube {
     public Cube3D getCenterPivot()
     {
         return cubies[CENTER_PIVOT_CUBIE_INDEX];
+    }
+
+    public void applyFullCubeRotation(Rotation rotation)
+    {
+        Cube3D[] transform = getFullCubeTransform(rotation);
+        applyRotation(rotation, cubies, transform);
+    }
+
+    public void applyMove(Move move)
+    {
+        Cube3D[] face = getFace(move);
+        Cube3D[] transform = getTransformedFace(face);
+        Rotation rotation = rotationsByMove.get(move);
+        applyRotation(rotation, face, transform);
+    }
+
+    public void applyInverseMove(Move move)
+    {
+        Cube3D[] face = getFace(move);
+        Cube3D[] transform = getInverseTransformedFace(face);
+        Rotation rotation = Rotation.getInverse(rotationsByMove.get(move));
+        applyRotation(rotation, face, transform);
+    }
+
+    private void applyRotation(Rotation rotation, Cube3D[] cubiesToRotate, Cube3D[] nextCubies)
+    {
+        for (int i = 0; i < cubiesToRotate.length; i++)
+        {
+            cubiesToRotate[i].applyRotation(rotation, nextCubies[i]);
+        }
+    }
+
+    private Cube3D[] getFullCubeTransform(Rotation rotation)
+    {
+        Cube3D[] transform = new Cube3D[CUBIES_PER_CUBE];
+        switch (rotation)
+        {
+            case UP:
+                transform[0] = new Cube3D(cubies[18]);
+                transform[1] = new Cube3D(cubies[19]);
+                transform[2] = new Cube3D(cubies[20]);
+                transform[3] = new Cube3D(cubies[9]);
+                transform[4] = new Cube3D(cubies[10]);
+                transform[5] = new Cube3D(cubies[11]);
+                transform[6] = new Cube3D(cubies[0]);
+                transform[7] = new Cube3D(cubies[1]);
+                transform[8] = new Cube3D(cubies[2]);
+
+                transform[9] = new Cube3D(cubies[21]);
+                transform[10] = new Cube3D(cubies[22]);
+                transform[11] = new Cube3D(cubies[23]);
+                transform[12] = new Cube3D(cubies[12]);
+                transform[13] = new Cube3D(cubies[13]);
+                transform[14] = new Cube3D(cubies[14]);
+                transform[15] = new Cube3D(cubies[3]);
+                transform[16] = new Cube3D(cubies[4]);
+                transform[17] = new Cube3D(cubies[5]);
+
+                transform[18] = new Cube3D(cubies[24]);
+                transform[19] = new Cube3D(cubies[25]);
+                transform[20] = new Cube3D(cubies[26]);
+                transform[21] = new Cube3D(cubies[15]);
+                transform[22] = new Cube3D(cubies[16]);
+                transform[23] = new Cube3D(cubies[17]);
+                transform[24] = new Cube3D(cubies[6]);
+                transform[25] = new Cube3D(cubies[7]);
+                transform[26] = new Cube3D(cubies[8]);
+                break;
+            case DOWN:
+                transform[0] = new Cube3D(cubies[6]);
+                transform[1] = new Cube3D(cubies[7]);
+                transform[2] = new Cube3D(cubies[8]);
+                transform[3] = new Cube3D(cubies[15]);
+                transform[4] = new Cube3D(cubies[16]);
+                transform[5] = new Cube3D(cubies[17]);
+                transform[6] = new Cube3D(cubies[24]);
+                transform[7] = new Cube3D(cubies[25]);
+                transform[8] = new Cube3D(cubies[26]);
+
+                transform[9] = new Cube3D(cubies[3]);
+                transform[10] = new Cube3D(cubies[4]);
+                transform[11] = new Cube3D(cubies[5]);
+                transform[12] = new Cube3D(cubies[12]);
+                transform[13] = new Cube3D(cubies[13]);
+                transform[14] = new Cube3D(cubies[14]);
+                transform[15] = new Cube3D(cubies[21]);
+                transform[16] = new Cube3D(cubies[22]);
+                transform[17] = new Cube3D(cubies[23]);
+
+                transform[18] = new Cube3D(cubies[0]);
+                transform[19] = new Cube3D(cubies[1]);
+                transform[20] = new Cube3D(cubies[2]);
+                transform[21] = new Cube3D(cubies[9]);
+                transform[22] = new Cube3D(cubies[10]);
+                transform[23] = new Cube3D(cubies[11]);
+                transform[24] = new Cube3D(cubies[18]);
+                transform[25] = new Cube3D(cubies[19]);
+                transform[26] = new Cube3D(cubies[20]);
+                break;
+            case CLOCK:
+                transform[0] = new Cube3D(cubies[18]);
+                transform[1] = new Cube3D(cubies[9]);
+                transform[2] = new Cube3D(cubies[0]);
+                transform[3] = new Cube3D(cubies[21]);
+                transform[4] = new Cube3D(cubies[12]);
+                transform[5] = new Cube3D(cubies[3]);
+                transform[6] = new Cube3D(cubies[24]);
+                transform[7] = new Cube3D(cubies[15]);
+                transform[8] = new Cube3D(cubies[6]);
+
+                transform[9] = new Cube3D(cubies[19]);
+                transform[10] = new Cube3D(cubies[10]);
+                transform[11] = new Cube3D(cubies[1]);
+                transform[12] = new Cube3D(cubies[22]);
+                transform[13] = new Cube3D(cubies[13]);
+                transform[14] = new Cube3D(cubies[4]);
+                transform[15] = new Cube3D(cubies[25]);
+                transform[16] = new Cube3D(cubies[16]);
+                transform[17] = new Cube3D(cubies[7]);
+
+                transform[18] = new Cube3D(cubies[20]);
+                transform[19] = new Cube3D(cubies[11]);
+                transform[20] = new Cube3D(cubies[2]);
+                transform[21] = new Cube3D(cubies[23]);
+                transform[22] = new Cube3D(cubies[14]);
+                transform[23] = new Cube3D(cubies[5]);
+                transform[24] = new Cube3D(cubies[26]);
+                transform[25] = new Cube3D(cubies[17]);
+                transform[26] = new Cube3D(cubies[8]);
+                break;
+            case COUNTER_CLOCK:
+                transform[0] = new Cube3D(cubies[2]);
+                transform[1] = new Cube3D(cubies[11]);
+                transform[2] = new Cube3D(cubies[20]);
+                transform[3] = new Cube3D(cubies[5]);
+                transform[4] = new Cube3D(cubies[14]);
+                transform[5] = new Cube3D(cubies[23]);
+                transform[6] = new Cube3D(cubies[8]);
+                transform[7] = new Cube3D(cubies[17]);
+                transform[8] = new Cube3D(cubies[26]);
+
+                transform[9] = new Cube3D(cubies[1]);
+                transform[10] = new Cube3D(cubies[10]);
+                transform[11] = new Cube3D(cubies[19]);
+                transform[12] = new Cube3D(cubies[4]);
+                transform[13] = new Cube3D(cubies[13]);
+                transform[14] = new Cube3D(cubies[22]);
+                transform[15] = new Cube3D(cubies[7]);
+                transform[16] = new Cube3D(cubies[16]);
+                transform[17] = new Cube3D(cubies[25]);
+
+                transform[18] = new Cube3D(cubies[0]);
+                transform[19] = new Cube3D(cubies[9]);
+                transform[20] = new Cube3D(cubies[18]);
+                transform[21] = new Cube3D(cubies[3]);
+                transform[22] = new Cube3D(cubies[12]);
+                transform[23] = new Cube3D(cubies[21]);
+                transform[24] = new Cube3D(cubies[6]);
+                transform[25] = new Cube3D(cubies[15]);
+                transform[26] = new Cube3D(cubies[24]);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid full cube rotation: " + rotation);
+        }
+        return transform;
     }
 
     private Cube3D[] getFace(Move move)
@@ -164,36 +337,6 @@ public class Cube {
         transform[7] = new Cube3D(face[3]);
         transform[8] = new Cube3D(face[6]);
         return transform;
-    }
-
-    public void applyRotation(Rotation rotation)
-    {
-        applyRotation(rotation, cubies, cubies);
-        // TODO: Make this rotation work after moves have been applied!!
-    }
-
-    private void applyRotation(Rotation rotation, Cube3D[] cubiesToRotate, Cube3D[] nextCubies)
-    {
-        for (int i = 0; i < cubiesToRotate.length; i++)
-        {
-            cubiesToRotate[i].applyRotation(rotation, nextCubies[i]);
-        }
-    }
-
-    public void applyMove(Move move)
-    {
-        Cube3D[] face = getFace(move);
-        Cube3D[] transform = getTransformedFace(face);
-        Rotation rotation = rotationsByMove.get(move);
-        applyRotation(rotation, face, transform);
-    }
-
-    public void applyInverseMove(Move move)
-    {
-        Cube3D[] face = getFace(move);
-        Cube3D[] transform = getInverseTransformedFace(face);
-        Rotation rotation = Rotation.getInverse(rotationsByMove.get(move));
-        applyRotation(rotation, face, transform);
     }
 
 }
