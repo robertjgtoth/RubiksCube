2e63d4f52129c85977f4c37ac0f8ebc1f913d6da
robertjgtoth <robertjgtoth@gmail.com>
2014-12-02 17:38:17 -0500

    Added random moves to support scrambling

--- src/main/java/com/robertjgtoth/rubiks/model/Move.java
+++ src/main/java/com/robertjgtoth/rubiks/model/Move.java
@@ -1,5 +1,7 @@
 package com.robertjgtoth.rubiks.model;
 
+import java.util.Random;
+
 /**
  * Created by rtoth on 11/26/2014.
  */
@@ -7,7 +9,31 @@ public enum Move {
 
     UP, DOWN, LEFT, RIGHT, FRONT, BACK;
 
+    private static final Random random = new Random();
+
     private Move()
     {}
+
+    public static Move getRandomMove()
+    {
+        int val = random.nextInt(6);
+        switch (val)
+        {
+            case 0:
+                return UP;
+            case 1:
+                return DOWN;
+            case 2:
+                return LEFT;
+            case 3:
+                return RIGHT;
+            case 4:
+                return FRONT;
+            case 5:
+                return BACK;
+            default:
+                return RIGHT;
+        }
+    }
 
 }
